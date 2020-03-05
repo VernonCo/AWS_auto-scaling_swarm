fd608fc800ef838b23354df8a0b572bccf898292
stuartz-VernonCo <stuartz@Vernoncompany.com>
2020-03-05 08:18:04 -0600

    updated token secret to include host:port

--- update_tokens.sh
+++ update_tokens.sh
@@ -4,5 +4,5 @@ docker swarm join-token --rotate manager
 docker swarm join-token --rotate worker
 
 # update tokens in secrets
-aws secretsmanager update-secret --secret-id $SWARM_MASTER_TOKEN --secret-string $(docker swarm join-token manager -q) 2>/dev/null
-aws secretsmanager update-secret --secret-id $SWARM_WORKER_TOKEN --secret-string $(docker swarm join-token worker -q) 2>/dev/null
+aws secretsmanager update-secret --secret-id $SWARM_MASTER_TOKEN --secret-string "$(docker swarm join-token manager -q) $(hostname):2377"
+aws secretsmanager update-secret --secret-id $SWARM_WORKER_TOKEN --secret-string "$(docker swarm join-token worker -q) $(hostname):2377"
